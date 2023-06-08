09117df1bc980b27c6c4a894d238d7d1b79f5c00
Lucky Hudda <113304073+Luckyhudda@users.noreply.github.com>
2023-06-08 22:18:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const box =document.querySelector('#box');
-const url = "https://jsonplaceholder.typicode.com/todos"
+const url = "https://jsonplaceholder.typicode.com/todos";
 
 function finalResult(data) {
   data.forEach((el)=>{
@@ -7,17 +7,17 @@ function finalResult(data) {
      let para1 = document.createElement('p');
      let para2 = document.createElement('p');
      let btn = document.createElement('button');
-     btn.classList.add('btn')
+     btn.classList.add('btn');
      div.classList.add('divData');
       btn.textContent = 'Click Me';
      para1.textContent = el.title;
      para2.textContent = el.completed;
-    para2.classList.add(el.completed === true ? 'green' : 'red')
+    para2.classList.add(el.completed === true ? 'green' : 'red');
 
     
    div.append(para1,para2);
-   div.appendChild(btn)
-   box.append(div)
+   div.appendChild(btn);
+   box.append(div);
 
    btn.addEventListener('click', function(){
     let randomColor1 = Math.trunc(Math.random() * 255) +1;
@@ -33,7 +33,7 @@ async function getData(){
   try{
     let data = await fetch(url);
     let finalData = await data.json();
-    finalResult(finalData)
+    finalResult(finalData);
   }catch{
     console.log('Error')
   }
